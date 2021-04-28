883787f7dbbf77345430eb9a172a89b21e6a98e3
Syed Areeb <syedraeebshah806@gmail.com>
2021-04-28 22:23:19 +0500

    Checking auto depoy

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React From Me
         </a>
       </header>
     </div>
