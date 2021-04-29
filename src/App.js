2310612273ccee2129e0ace78dc026e760d626db
Syed Areeb <syedraeebshah806@gmail.com>
2021-04-29 12:30:48 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React From Me
+          Learn React From Areeb
         </a>
       </header>
     </div>
